a80e52bddac7ff7a296e403145daf74652ebde88
PeterShevchuk <raf02041994@gmail.com>
2020-06-19 12:05:55 +0300

    end

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -10293,8 +10293,8 @@ $(document).ready(function () {
     arrows: true,
     infinite: true,
     speed: 300,
-    prevArrow: '<button class="slider__icon-prev"><svg class=""> <use xlink:href="../images/svg/slider/sprite.svg#icon-prev"></use></svg></button>',
-    nextArrow: '<button class="slider__icon-next"><svg class=""><use xlink:href="../images/svg/slider/sprite.svg#icon-next"></use></svg></button>',
+    prevArrow: '<button class="slider__icon-prev"><svg class=""> <use xlink:href="./images/svg/slider/sprite.svg#icon-prev"></use></svg></button>',
+    nextArrow: '<button class="slider__icon-next"><svg class=""><use xlink:href="./images/svg/slider/sprite.svg#icon-next"></use></svg></button>',
     responsive: [{
       breakpoint: 4000,
       settings: {
